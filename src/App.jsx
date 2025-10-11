42f784491c2fe9c5b7e3d27351e1d303f5bdafab
tomjid <tomjidhuda04@gmail.com>
2025-10-11 22:53:02 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <h1>hello</h1>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
